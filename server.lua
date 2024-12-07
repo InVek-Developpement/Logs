a87abf8b1484a8ba4f52eeaac2867471c6c6d094
InVek <68790635+InVekYT@users.noreply.github.com>
2024-12-07 22:29:56 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,22 +1,27 @@
-local date = os.date("*t")
+local function LogsDiscordScript(nom, titre, desc, color, webhook)
+    if not webhook then return end
 
-if date.day < 10 then date.day = '0' .. tostring(date.day) end
-if date.month < 10 then date.month = '0' .. tostring(date.month) end
-if date.hour < 10 then date.hour = '0' .. tostring(date.hour) end
-if date.min < 10 then date.min = '0' .. tostring(date.min) end
-if date.sec < 10 then date.sec = '0' .. tostring(date.sec) end
+    local date = os.date("*t")
+    local formattedDate = string.format("%02d/%02d/%04d à %02dh%02dm%02ds", date.day, date.month, date.year, date.hour, date.min, date.sec)
 
-local function LogsDiscordScript(nom, titre, desc, webhook)
+    nom = nom or "Bot Logs"
+    titre = titre or "Titre"
+    desc = desc or "..."
+    color = color or 0
+
     local embed = {
         {
-            ["color"] = 0,
+            ["color"] = color,
             ["title"] = titre,
             ["description"] = desc,
             ["footer"] = {
-                ["text"] = ("%s/%s/%s à %sh%sm %ss"):format(date.day, date.month, date.year, date.hour, date.min, date.sec),
+                ["text"] = formattedDate,
                 ["icon_url"] = nil
             }
         }
     }
-    PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({username = nom, embeds = embed}), { ['Content-Type'] = 'application/json' })
+
+    PerformHttpRequest(webhook, function() end, 'POST', json.encode({ username = nom, embeds = embed}), { ['Content-Type'] = 'application/json' })
 end
+
+exports('LOGS_DISCORD', LogsDiscordScript)
